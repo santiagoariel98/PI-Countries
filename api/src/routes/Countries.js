4255d65c430c9ae214d104fb06eef106d411746b
santiagoariel98 <santiagovillanuevaariel@gmail.com>
2022-02-01 20:22:58 -0300

    deploy

--- api/src/routes/Countries.js
+++ api/src/routes/Countries.js
@@ -1,60 +1,59 @@
-const router = require('express').Router();
-const axios = require("axios")
+const router = require("express").Router();
+const axios = require("axios");
 const { Sequelize, Model, DataTypes } = require("sequelize");
-const {Country,Activities} = require('../db.js');
+const { Country, Activities } = require("../db.js");
 const Op = Sequelize.Op;
 
-  let restAPI = axios("https://restcountries.com/v3/all").then((e)=> e.data.map(e=>{
-      Country.findOrCreate({
-        where: {id: e.cca3},
-        defaults: {
-          id: e.cca3,
-          name: e.name.common,
-          flags: e.flags[0],
-          continent: e.region,
-          capital: e.capital? e.capital[0]: "-",
-          subregion: e.subregion || "-",
-          area: e.area || 1,
-          population: e.population || 0,
-          maps: e.maps.googleMaps
-        }
-      })
-  }))
+// let restAPI = axios("https://restcountries.com/v3/all").then((e) =>
+//   e.data.map((e) => {
+//     Country.findOrCreate({
+//       where: { id: e.cca3 },
+//       defaults: {
+//         id: e.cca3,
+//         name: e.name.common,
+//         flags: e.flags[0],
+//         continent: e.region,
+//         capital: e.capital ? e.capital[0] : "-",
+//         subregion: e.subregion || "-",
+//         area: e.area || 1,
+//         population: e.population || 0,
+//         maps: e.maps.googleMaps,
+//       },
+//     });
+//   })
+// );
 
-router.get("/countries", async (req,res)=>{
-  
+router.get("/countries", async (req, res) => {
   const db = await Country.findAll({
     include: {
-      model: Activities
-    }
-  })
+      model: Activities,
+    },
+  });
   // query filter
-  const name = req.query.name
+  const name = req.query.name;
   // query filter
 
-  if(name){
-      let dataQuery = db.filter(e=> e.dataValues.name.toLowerCase().includes(name.toLowerCase()))
-      return dataQuery.length? res.json(dataQuery): res.sendStatus(404)
+  if (name) {
+    let dataQuery = db.filter((e) =>
+      e.dataValues.name.toLowerCase().includes(name.toLowerCase())
+    );
+    return dataQuery.length ? res.json(dataQuery) : res.sendStatus(404);
+  } else {
+    res.send(db);
   }
-  else{
-      res.send(db)
-  } 
-})
-
-
-
-router.get("/countries/:id", async (req,res)=>{
-  const { id } = req.params
-  if(id.length !== 3 || id.match(/^[0-9]+$/)) return res.sendStatus(404)
-  const country = await Country.findByPk(id.toUpperCase(),{
-  include: [{
-    model: Activities
-  }]
 });
-country? res.json(country): res.sendStatus(404)
-})
 
-
-
+router.get("/countries/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id.length !== 3 || id.match(/^[0-9]+$/)) return res.sendStatus(404);
+  const country = await Country.findByPk(id.toUpperCase(), {
+    include: [
+      {
+        model: Activities,
+      },
+    ],
+  });
+  country ? res.json(country) : res.sendStatus(404);
+});
 
 module.exports = router;
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,79 +8,81 @@ export const POST_ADD_ACTIVITIES = "POST_ADD_ACTIVITIES";
 export const GET_FILTERS = "GET_FILTERS";
 
 export function getCountries() {
-	return (dispatch) => {
-		return axios
-			.get("http://localhost:3001/countries")
-			.then((res) => {
-				let countries = res.data.map((e) => {
-					let country = {
-						id: e.id,
-						img: e.flags,
-						name: e.name,
-						continent: e.continent,
-						population: e.population,
-						Activities: e.Activities,
-						maps: e.maps,
-					};
-					return country;
-				});
-				dispatch({
-					type: GET_COUNTRIES,
-					payload: countries,
-				});
-			})
-			.catch((e) => e);
-	};
+  return (dispatch) => {
+    return axios
+      .get("https://countries-app-2021.herokuapp.com/countries")
+      .then((res) => {
+        let countries = res.data.map((e) => {
+          let country = {
+            id: e.id,
+            img: e.flags,
+            name: e.name,
+            continent: e.continent,
+            population: e.population,
+            Activities: e.Activities,
+            maps: e.maps,
+          };
+          return country;
+        });
+        dispatch({
+          type: GET_COUNTRIES,
+          payload: countries,
+        });
+      })
+      .catch((e) => e);
+  };
 }
 
 export function getCountryById(id) {
-	return async (dispatch) => {
-		let restDB = await axios
-			.get(`http://localhost:3001/countries/${id}`)
-			.then()
-			.catch((e) => e);
-		return dispatch({
-			type: GET_COUNTRIES_BY_ID,
-			payload: restDB.data,
-		});
-	};
+  return async (dispatch) => {
+    let restDB = await axios
+      .get(`https://countries-app-2021.herokuapp.com/countries/${id}`)
+      .then()
+      .catch((e) => e);
+    return dispatch({
+      type: GET_COUNTRIES_BY_ID,
+      payload: restDB.data,
+    });
+  };
 }
 
 export function getCountriesName(name) {
-	return async (dispatch) => {
-		let restDB = await axios
-			.get(`http://localhost:3001/countries?name=${name}`)
-			.catch((e) => e);
-		return dispatch({
-			type: GET_COUNTRIES_NAME,
-			payload: restDB.data,
-		});
-	};
+  return async (dispatch) => {
+    let restDB = await axios
+      .get(`https://countries-app-2021.herokuapp.com/countries?name=${name}`)
+      .catch((e) => e);
+    return dispatch({
+      type: GET_COUNTRIES_NAME,
+      payload: restDB.data,
+    });
+  };
 }
 
 export function getActivities() {
-	return async (dispatch) => {
-		let restDB = await axios.get(`http://localhost:3001/activities`);
-		return dispatch({
-			type: GET_ACTIVITIES,
-			payload: restDB.data,
-		});
-	};
+  return async (dispatch) => {
+    let restDB = await axios.get(
+      `https://countries-app-2021.herokuapp.com/activities`
+    );
+    return dispatch({
+      type: GET_ACTIVITIES,
+      payload: restDB.data,
+    });
+  };
 }
 
 export function postAddActivities(payload) {
-	return async (dispatch) => {
-		let restDB = await axios.post(
-			`http://localhost:3001/activity`,
-			payload
-		);
-		return restDB;
-	};
+  return async (dispatch) => {
+    let restDB = await axios.post(
+      `https://countries-app-2021.herokuapp.com/activity`,
+      payload
+    );
+    return restDB;
+  };
 }
 
 export function getFilters(payload) {
-	return {
-		type: "GET_FILTERS",
-		payload,
-	};
+  return {
+    type: "GET_FILTERS",
+    payload,
+  };
 }
